f5977d6647e9b0e0d41d4b3d7153dff417153f42
Ethan <noxinnahte@gmail.com>
2021-04-25 14:51:24 -0400

    Update TODO

--- src/CodingFundamentalsDriver.java
+++ src/CodingFundamentalsDriver.java
@@ -5,6 +5,8 @@ public class CodingFundamentalsDriver
     {
         /*
          TODO:add boolean expressions
+          Abstract class vs Interface. There is some shared functionality between the question classes but
+          an abstract class doesn't work with the quiz class arraylist
             At some point: log results to a text file with date and time, name, num of questions, score, correct answer/their answer
         */
         Menu menu = new Menu();
